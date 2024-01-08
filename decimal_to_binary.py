a6f6c2e106f7a56549b81306e6b560536cf0a663
Debobrata Dutta <142308382+Debobrata-Dutta@users.noreply.github.com>
2024-01-08 19:17:58 +0530

    Create decimal_to_binary.py

---
+++ decimal_to_binary.py
@@ -0,0 +1,12 @@
+# Python program to convert Decimal to binary number
+rem1=0
+num=89
+while(num>0):
+    rem=num%2
+    num=num//2
+    rem1=rem1*10+rem
+rem2=0
+rem2=str(rem1)
+size=len(rem2)
+reverse=rem2[size::-1]
+print(reverse)
